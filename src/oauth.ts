a559a1d2fa6dec7e91f2bed3c216ca930f2c0fe8
Marcus <mattosse@hotmail.com>
2017-02-18 17:27:28 +0100

    Finished oauth automation

--- src/oauth.ts
+++ src/oauth.ts
@@ -2,11 +2,22 @@ import uuid = require('uuid/v4');
 import express = require('express')  
 import debug = require('debug');
 import request = require('request');
-import Nightmare = require('nightmare');       
+
+import Nightmare = require('nightmare');   
+import phantom = require('phantom');
+import Horseman = require('node-horseman');
+import fs = require('fs');    
 
 interface ITwitchResponse {
     access_token: string,
     scope: string
+}
+
+interface IAutomation {
+    user: string,
+    password: string,
+    show?: boolean,
+    verify?: boolean
 }
 
 export interface IOptions {
@@ -16,9 +27,7 @@ export interface IOptions {
     client_id: string,
     client_secret: string,
     server?: boolean,
-    automated?: boolean,
-    user?: string,
-    password?: string
+    automated?: IAutomation
 }
 
 export class Oauth {
@@ -30,6 +39,8 @@ export class Oauth {
     private _oauth_url_token: string = 'https://api.twitch.tv/kraken/oauth2/token';
     private _oauth_url_auth: string = 'https://api.twitch.tv/kraken/oauth2/authorize';
     private _scope: string;
+    private _automated: IAutomation;
+    private _server_listening: boolean;
 
     private _current_state: string = '';
     private app = express();  
@@ -47,24 +58,36 @@ export class Oauth {
         this._client_id = options.client_id || process.env.TWITCH_TOKEN;
         this._client_secret = options.client_secret || process.env.TWITCH_SECRET;
         this._scope = options.scope || process.env.TWITCH_SCOPE;
+        this._automated = options.automated;
 
         if (options.server) {
+            this._server_listening = true;
             this._debug('Starting server');
             this.StartServer();
         }
 
-        if (options.automated && options.user && options.password) {
+        if (options.automated && options.automated.user && options.automated.password) {
             this._debug(`Starting automated oauth login`);
 
             if (!options.server) {
-                this._debug('Starting server for automated login');
+                this._server_listening = true;
+                this._debug('Starting server for automated login, ready to call Automate()');
                 this.StartServer();
+                setTimeout(() => {
+                    this.Automate().then((data) => console.log(data)).catch((err) => console.error(err));
+                }, 1500);
             }
-
-            setTimeout(() => {
-                this.Automatelogin(options.user, options.password);
-            }, 1500);
         }
+    }
+
+    public Automate() {
+        return new Promise((resolve, reject) => {
+            if (this._automated && this._automated.user && this._automated.password && this._server_listening) {
+                this.Automatelogin(this._automated.user, this._automated.password, this._automated.show).then(resolve).catch(reject);
+            } else {
+                reject('Please modify your options to allow automation');
+            }
+        })
     }
 
     /**
@@ -100,8 +123,10 @@ export class Oauth {
 
     /**
      * Automate twitch login to grab the access token
-     * beaware that this page contains captcha protection
-     * consider using a proxy if multiple attemps are made
+     * be aware that this page contains captcha protection
+     * consider using a proxy if multiple attemps are to be made
+     * 
+     *  THIS IS VERY MESSY, BUT WORKS
      * 
      * @private
      * @param {string} user
@@ -110,26 +135,202 @@ export class Oauth {
      * 
      * @memberOf Oauth
      */
-    private Automatelogin (user: string, password: string, show?: false) {
+    private Automatelogin (user: string, password: string, show?: boolean) {
         return new Promise((resolve, reject) => {
-            const nightmare = Nightmare({ show: show });
+            
+            const nightmare = Nightmare({ show: show || false });
 
-            nightmare.goto(`http://localhost:${this._port}/auth`)
-                .wait('input[name="username"]')
-                .type('input[name="username"]', user)
-                .type('input[name="password"]', password)
-                .click('button')
-                .wait('twitch-data')
-                .evaluate(function () {
-                    return document.querySelector('twitch-data').textContent
-                })
-                .end()
-                .then(function (result) {
-                    resolve(JSON.parse(result));
-                })
-                .catch(function (error) {
-                    reject(error);
+            // TODO: .g-recaptcha detection
+
+            if (fs.existsSync('cookies.json')) {
+                fs.readFile('cookies.json', 'utf8', (err, data) => {
+                    if (err) return console.error('Unable to read cookies.json', err);
+
+                    const sesh = nightmare.goto(`http://localhost:${this._port}/auth`);
+
+                    // If we can read the cookie file we proceed
+                    if (typeof data === 'string') {
+                        this._debug('Found cookies');
+
+                        try {
+                            sesh
+                                .cookies.set(JSON.parse(data))
+                                .wait()
+                                .goto(`http://localhost:${this._port}/auth`)
+                        }catch (e) {
+                            return console.error('Unable to parse cookies.json');
+                        }
+                        
+                    } else {
+                        this._debug('Cookie file invalid, continuing with manual login');
+                    }
+
+                    sesh
+                        .exists('input[name="username"]').then((result) => {
+                            if (result) {
+                                this._debug('Username input exists, Proceeding with login')
+                                sesh
+                                    .exists('.g-recaptcha').then((result) => { // Looks like the captcha exists hmm.
+                                        if (result) {
+                                            if (this._automated.show) {
+                                                this._debug('Captcha has been activated, Solve it without clicking submit');
+
+                                                sesh
+                                                    .insert('input[name="username"]', user) // Insert for convencience
+                                                    .insert('input[name="password"]', password) // Insert for convencience
+
+                                                    // Wait for user to solve captcha ( 30s )
+                                                    .wait('.recaptcha-checkbox-checked') 
+                                                    // Check if its been solved
+                                                    .exists('.recaptcha-checkbox-checked', (result) => { 
+                                                        if (result) { // its been solved so we continue
+                                                            sesh
+                                                                .click('button')
+                                                                .wait()
+                                                                .exists('.js-authorize').then((result) => {
+                                                                    if (result) {
+                                                                        this._debug('Authorize button exists, clicking...');
+                                                                        sesh
+                                                                            .click('.js-authorize')
+                                                                            .wait('twitch-data')
+                                                                            .evaluate(() => {
+                                                                                return document.querySelector('twitch-data').textContent
+                                                                            })
+                                                                            .then((result) => {
+                                                                                sesh
+                                                                                    .cookies.get({url: null}).end().then((cookies) => {
+                                                                                        fs.writeFile('cookies.json', 
+                                                                                            JSON.stringify(cookies), (err) => {
+                                                                                                if (err) return console.log(err);
+                                                                                                this._debug('Writing cookies to file');
+                                                                                        });
+                                                                                    });
+
+                                                                                resolve(JSON.parse(result));
+                                                                            })
+                                                                            .catch((error) => {
+                                                                                reject(error);
+                                                                            });
+                                                                    } else {
+                                                                        sesh
+                                                                            .wait('twitch-data')
+                                                                            .evaluate(() => {
+                                                                                return document.querySelector('twitch-data').textContent
+                                                                            })
+                                                                            .then((result) => {
+                                                                                sesh
+                                                                                    .cookies.get({url: null}).end().then((cookies) => {
+                                                                                        fs.writeFile('cookies.json', 
+                                                                                            JSON.stringify(cookies), (err) => {
+                                                                                                if (err) return console.log(err);
+                                                                                                this._debug('Writing cookies to file');
+                                                                                        });
+                                                                                    });
+
+                                                                                resolve(JSON.parse(result));
+                                                                            })
+                                                                            .catch((error) => {
+                                                                                reject(error);
+                                                                            });
+                                                                    }
+                                                                });
+                                                        } else {
+                                                            sesh
+                                                                .halt('Captcha has been activated', () => {
+                                                                    return console.error(`Captcha has been activated.` 
+                                                                        + ` You failed to solve it within the allotted time (30s)`);
+                                                                }); // Kill it
+                                                        }
+                                                    });
+                                            } else {
+                                                sesh
+                                                    .halt('Captcha has been activated', () => {
+                                                        return console.error(`Captcha has been activated.` 
+                                                            + ` Please use the show option in automation to enter it manually` 
+                                                            + ` or use the proxy option.`);
+                                                    }); // Kill it
+                                            }
+                                        } else {
+                                            sesh
+                                                .insert('input[name="username"]', user)
+                                                .insert('input[name="password"]', password)
+                                                .click('button')
+                                                .wait()
+                                                .exists('.js-authorize').then((result) => {
+                                                    if (result) {
+                                                        this._debug('Authorize button exists, clicking...');
+                                                        sesh
+                                                            .click('.js-authorize')
+                                                            .wait('twitch-data')
+                                                            .evaluate(() => {
+                                                                return document.querySelector('twitch-data').textContent
+                                                            })
+                                                            .then((result) => {
+                                                                sesh
+                                                                    .cookies.get({url: null}).end().then((cookies) => {
+                                                                        fs.writeFile('cookies.json', JSON.stringify(cookies), (err) => {
+                                                                            if (err) return console.log(err);
+                                                                            this._debug('Writing cookies to file');
+                                                                        });
+                                                                    });
+
+                                                                resolve(JSON.parse(result));
+                                                            })
+                                                            .catch((error) => {
+                                                                reject(error);
+                                                            });
+                                                    } else {
+                                                        sesh
+                                                            .wait('twitch-data')
+                                                            .evaluate(() => {
+                                                                return document.querySelector('twitch-data').textContent
+                                                            })
+                                                            .then((result) => {
+                                                                sesh
+                                                                    .cookies.get({url: null}).end().then((cookies) => {
+                                                                        fs.writeFile('cookies.json', JSON.stringify(cookies), (err) => {
+                                                                            if (err) return console.log(err);
+                                                                            this._debug('Writing cookies to file');
+                                                                        });
+                                                                    });
+
+                                                                resolve(JSON.parse(result));
+                                                            })
+                                                            .catch((error) => {
+                                                                reject(error);
+                                                            });
+                                                    }
+                                                })
+                                        }
+                                    });
+                            } else {
+                                // Press authorize button
+                                this._debug('Authorize button exists, clicking...');
+                                sesh
+                                    .wait('.js-authorize')
+                                    .click('.js-authorize')
+                                    .wait('twitch-data')
+                                    .evaluate(() => {
+                                        return document.querySelector('twitch-data').textContent
+                                    })
+                                    .then((result) => {
+                                        sesh
+                                            .cookies.get({url: null}).end().then((cookies) => {
+                                                fs.writeFile('cookies.json', JSON.stringify(cookies), (err) => {
+                                                    if (err) return console.log(err);
+                                                    this._debug('Writing cookies to file');
+                                                });
+                                            });
+
+                                        resolve(JSON.parse(result));
+                                    })
+                                    .catch((error) => {
+                                        reject(error);
+                                    });
+                            }
+                        });
                 });
+            }
         });
     }
 
@@ -191,10 +392,17 @@ export class Oauth {
 
         this._current_state = uuid();
         this._scope = req.query.scope || this._scope;
+        let verify_auth: boolean = false;
+
+        // This parameter decides whether the user should be re-prompted for authorization.
+        // Aka click a button to proceed or not, this is required for the automated login
+        if (this._automated) { 
+            verify_auth = this._automated.verify;
+        }
 
         process.nextTick(() => 
             res.redirect(`${this._oauth_url_auth}?response_type=code&client_id=${this._client_id}` + 
-            `&redirect_uri=${this._url}&scope=${this._scope}&state=${this._current_state}`)
+            `&redirect_uri=${this._url}&scope=${this._scope}&state=${this._current_state}&force_verify=true`)
         ); 
     }
 }
